70c8ceb2e0245fe997fa0cfad143a901d79aa56e
George Pasley <gpasley@imazuma.com>
2016-03-06 23:27:10 -0500

    added check for unique email

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,30 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         
+  after_create :create_account
+  
+  validate :email_is_unique, on: :create
 
   def confirmation_required?
     false
   end
   
-           
+  private
+  
+  def email_is_unique
+    
+    return false unless self.errors[:email].empty?
+    
+    unless Account.find_by_email(email).nil?
+      errors.add(:email, " is already taken")
+    end
+  end
+  
+  def create_account
+    account = Account.new(email: email)
+    account.save!
+  end         
 
 end
 
